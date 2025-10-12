27dac69b376a9a6a34c05d76b6644f113256efe8
Kouji228 <song24707@gmail.com>
2025-10-13 02:13:56 +0800

    1013_網頁資料更新

--- src/content_option.tsx
+++ src/content_option.tsx
@@ -1,171 +1,142 @@
 const logotext = 'Kouji';
+
 const meta = {
-  title: 'Kouji Song',
-  description: 'I’m Kouji Song Front-End devloper,currently working in Taipei',
+  title: 'Kouji Song - Front-End Developer',
+  description:
+    "I'm Kouji Song, Front-End Developer based in New Taipei City, specializing in React.js/Next.js development",
 };
 
 const introdata = {
-  title: 'I’m Kouji',
+  title: "Hi, I'm Kouji Song",
   animated: {
     first: '資訊工程小白',
     second: '前端工程師',
     third: '日語使用者',
-    forth: '偶爾是籃球員',
+    forth: '貓派！',
   },
   description:
-    '自學前端技術，目前參與政府資助專案的前端開發，熱衷於新技術與學習，對資訊工程領域亦充滿熱情，2025/1養了一隻英國藍貓Charmmy。',
+    '正積極轉職為前端工程師，擁有廣告數據分析背景，培養了嚴謹的邏輯思維與問題解決能力。專注於 React.js/Next.js 開發，具備購物車、會員系統與 API 串接經驗，重視使用者體驗與效能優化。持續投入專案實作與新技術學習，期望以跨領域思維與實作力，成為能為團隊帶來價值的前端工程師。',
   your_img_url: '/picture/cat2.JPG',
 };
 
 const dataabout = {
-  title: 'About my self',
+  title: 'About Myself',
   aboutme:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.',
+    '我是宋平浩（Kouji Song），擁有廣告數據分析背景，目前正積極投入前端工程師職涯發展。從廣告成效分析工作中，深刻體會到程式設計與前端技術的重要性，驅動我展開轉職之路。透過資展國際半年制 Bootcamp 密集學習與專案開發，紮實建立了 React/Next.js 技術基礎。具備 3 年以上日商體系任職經驗，累積跨部門協作與數據導向的問題解決經驗。擁有英日雙語能力（TOEIC 735、JLPT N1），能有效進行跨文化溝通。擅長將需求有效拆解為可交付任務，並在短週期內持續優化體驗與效能。期待以跨領域思維與實作力，成為能為團隊帶來價值的前端工程師。',
 };
+
 const worktimeline = [
   {
-    jobtitle: 'Designer of week',
-    where: 'YAdfi',
-    date: '2020',
+    jobtitle: '前端工程師培訓',
+    where: '資展國際股份有限公司',
+    date: '2025/4 - 2025/9',
   },
   {
-    jobtitle: 'Designer of week',
-    where: 'Jamalya',
-    date: '2019',
+    jobtitle: '廣告AE業務',
+    where: '台灣博報堂股份有限公司',
+    date: '2021/5 - 2025/3',
   },
   {
-    jobtitle: 'Designer of week',
-    where: 'ALquds',
-    date: '2019',
+    jobtitle: '網站編輯實習生',
+    where: '菁華出版社',
+    date: '2020/7 - 2020/10',
   },
 ];
 
 const skills = [
   {
-    name: 'Html',
-    value: 50,
+    name: 'HTML5',
+    value: 85,
   },
   {
-    name: 'CSS',
-    value: 50,
+    name: 'CSS3',
+    value: 80,
   },
   {
-    name: 'Javascript',
-    value: 50,
+    name: 'JavaScript (ES6+)',
+    value: 80,
   },
   {
-    name: 'React',
-    value: 50,
+    name: 'React.js',
+    value: 75,
   },
   {
     name: 'Next.js',
-    value: 50,
+    value: 75,
+  },
+  {
+    name: 'Node.js / Express',
+    value: 65,
+  },
+  {
+    name: 'Bootstrap',
+    value: 70,
+  },
+  {
+    name: 'Git / GitHub',
+    value: 80,
+  },
+  {
+    name: 'MySQL / MariaDB',
+    value: 65,
   },
   {
     name: 'Python',
-    value: 50,
+    value: 60,
+  },
+  {
+    name: 'PHP',
+    value: 55,
   },
 ];
 
 const services = [
   {
-    title: 'UI & UX Design',
+    title: '前端開發',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+      '使用 React.js / Next.js 建立響應式網站，具備購物車、會員系統與 API 串接經驗，重視使用者體驗與效能優化。',
   },
   {
-    title: 'Mobile Apps',
+    title: '跨文化溝通',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+      '具備英日雙語能力（TOEIC 735、JLPT N1），能有效進行跨文化溝通，適合國際團隊協作或日本市場專案。',
   },
   {
-    title: 'Wordpress Design',
+    title: '數據分析思維',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+      '擁有廣告數據分析背景，培養了嚴謹的邏輯思維與問題解決能力，能將數據洞察轉化為產品優化方案。',
   },
 ];
 
 const dataportfolio = [
   {
-    img: 'https://picsum.photos/400/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
+    img: '/picture/achievement_img_1759460471921.webp',
+    description: '黑膠商品販售電商平台 - Next.js、Node.js、MySQL、金流串接',
     link: '#',
   },
   {
-    img: 'https://picsum.photos/400/800/?grayscale',
+    img: '/picture/achievement_img_1759461756480.webp',
     description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
+      'MedSelect 抗癌藥物臨床前療效測試輔助平台 - Next.js、Chart.js、RESTful API',
+    link: 'https://innoaward.taiwan-healthcare.org/award_detail.php?REFDOCTYPID=0mge2qqssdm4fd72&num=1&REFDOCID=0sm1tc9ylyod3vh1',
   },
   {
-    img: 'https://picsum.photos/400/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
+    img: '/picture/achievement_img_1759913318760.webp',
+    description: '黑膠販售電商 Dashboard - PHP、MySQL、CKEditor',
+    link: 'https://github.com/Kouji228/Vinyl_Shop_Dashboard',
   },
   {
-    img: 'https://picsum.photos/400/600/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
-  },
-  {
-    img: 'https://picsum.photos/400/300/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
-  },
-  {
-    img: 'https://picsum.photos/400/700/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
-  },
-
-  {
-    img: 'https://picsum.photos/400/600/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
-  },
-  {
-    img: 'https://picsum.photos/400/300/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
-  },
-  {
-    img: 'https://picsum.photos/400/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
-  },
-  {
-    img: 'https://picsum.photos/400/550/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
-  },
-  {
-    img: 'https://picsum.photos/400/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
-  },
-  {
-    img: 'https://picsum.photos/400/700/?grayscale',
-    description:
-      'The wisdom of life consists in the elimination of non-essentials.',
-    link: '#',
+    img: 'https://picsum.photos/400/300?random=4',
+    description: '個人網站 - Next.js 15 + TypeScript + Tailwind CSS',
+    link: 'https://github.com/Kouji228/kouji_personal_website',
   },
 ];
 
 const contactConfig = {
-  YOUR_EMAIL: 'name@domain.com',
-  YOUR_FONE: 'Nan',
+  YOUR_EMAIL: 'song24707@gmail.com',
+  YOUR_FONE: '0965-740-673',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+    '歡迎與我聯繫討論前端開發、技術合作或任何有趣的專案！平日 9:00-18:00 可手機或 Email 聯絡，18:00 後及週末建議以 Email 聯繫。',
   // creat an emailjs.com account
   // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
   YOUR_SERVICE_ID: 'service_id',
@@ -174,11 +145,12 @@ const contactConfig = {
 };
 
 const socialprofils = {
-  github: 'https://github.com',
-  facebook: 'https://facebook.com',
+  github: 'https://github.com/Kouji228',
   linkedin: 'https://linkedin.com',
+  facebook: 'https://facebook.com',
   twitter: 'https://twitter.com',
 };
+
 export {
   meta,
   dataabout,
